26af19a90fa53e2a3d7e963e1b03917f20898234
Han <hanhhan2705@gmail.com>
2026-07-10 21:24:54 +1000

    move Parser and Event functions to cpp files

--- include/Parser.hpp
+++ include/Parser.hpp
@@ -5,26 +5,9 @@
 
 class Parser {
     public:
-        explicit Parser(std::string filename) {
-            std::ifstream file(filename);
-
-            if (!file.is_open()) {
-                throw std::invalid_argument(std::format("Cannot open the file with this name: {}", filename));
-            }
-            
-            std::string line;
-            while (getline(file, line)) {
-                logs.push_back(line);
-            }
-        }
-
-        std::vector<std::string> getLogs() {
-            return logs;
-        }
-
-        size_t getLogsSize() {
-            return logs.size();
-        }
+        explicit Parser(std::string filename);
+        std::vector<std::string> getLogs();
+        size_t getLogsSize();
     private:
         std::vector<std::string> logs;
 };
---
+++ src/Parser.cpp
@@ -0,0 +1,22 @@
+#include "Parser.hpp"
+
+Parser::Parser(std::string filename) {
+    std::ifstream file(filename);
+
+    if (!file.is_open()) {
+        throw std::invalid_argument(std::format("Cannot open the file with this name: {}", filename));
+    }
+    
+    std::string line;
+    while (getline(file, line)) {
+        logs.push_back(line);
+    }
+}
+
+std::vector<std::string> Parser::getLogs() {
+    return logs;
+}
+
+size_t Parser::getLogsSize() {
+    return logs.size();
+}
