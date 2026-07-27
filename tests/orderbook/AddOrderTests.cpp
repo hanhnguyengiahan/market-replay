954b25d9633c584bbf3fa3a34650e6bc427b2acb
Han <hanhhan2705@gmail.com>
2026-07-27 16:31:05 +1000

    refactor: separate test into each test file

--- tests/orderbook/AddOrderTests.cpp
+++ tests/orderbook/AddOrderTests.cpp
@@ -1,7 +1,7 @@
 #include "OrderBook.hpp"
 #include <gtest/gtest.h>
 
-class OrderBookTest : public testing::Test {
+class AddOrderTest : public testing::Test {
   protected:
     Order createOrder(orderId_t id, price_t price, quantity_t quantity, side_t side) {
         return Order(id, 1000000 + id, price, quantity, "AAPL", side);
@@ -9,13 +9,12 @@ class OrderBookTest : public testing::Test {
     OrderBook orderBook;
 };
 
-// Add Order
-TEST_F(OrderBookTest, EmptyBookHasNoPriceLevels) {
+TEST_F(AddOrderTest, EmptyBookHasNoPriceLevels) {
     EXPECT_TRUE(orderBook.getPriceLevels("BUY").empty());
     EXPECT_TRUE(orderBook.getPriceLevels("SELL").empty());
 }
 
-TEST_F(OrderBookTest, BuyAndSellOrdersStoredSeparately) {
+TEST_F(AddOrderTest, BuyAndSellOrdersStoredSeparately) {
     auto buy = createOrder(1, 150.25, 100, "BUY");
     auto sell = createOrder(2, 150.30, 100, "SELL");
 
@@ -29,7 +28,7 @@ TEST_F(OrderBookTest, BuyAndSellOrdersStoredSeparately) {
               (std::vector<std::pair<price_t, quantity_t>>{{150.30, 100}}));
 }
 
-TEST_F(OrderBookTest, QuantityAccumulatedIfSamePrice) {
+TEST_F(AddOrderTest, QuantityAccumulatedIfSamePrice) {
     auto order1 = createOrder(1, 150.25, 100, "BUY");
     auto order2 = createOrder(2, 150.25, 100, "BUY");
     auto order3 = createOrder(3, 150.30, 100, "BUY");
@@ -42,7 +41,7 @@ TEST_F(OrderBookTest, QuantityAccumulatedIfSamePrice) {
               (std::vector<std::pair<price_t, quantity_t>>{{150.30, 100}, {150.25, 200}}));
 }
 
-TEST_F(OrderBookTest, HighestBidsReturnFromPriceLevels) {
+TEST_F(AddOrderTest, HighestBidsReturnFromPriceLevels) {
     Order order1 = createOrder(1, 150.25, 100, "BUY");
     Order order2 = createOrder(2, 150.30, 100, "BUY");
 
@@ -53,7 +52,7 @@ TEST_F(OrderBookTest, HighestBidsReturnFromPriceLevels) {
     EXPECT_EQ(orderBook.getPriceLevels("BUY"), expected);
 }
 
-TEST_F(OrderBookTest, LowestSellsReturnFromPriceLevels) {
+TEST_F(AddOrderTest, LowestSellsReturnFromPriceLevels) {
     Order order1 = createOrder(1, 150.20, 100, "SELL");
     Order order2 = createOrder(1, 150.25, 100, "SELL");
     Order order3 = createOrder(2, 150.30, 100, "SELL");
@@ -68,7 +67,7 @@ TEST_F(OrderBookTest, LowestSellsReturnFromPriceLevels) {
 }
 
 // Trade
-TEST_F(OrderBookTest, NoTradeWhenPricesDoNotCross) {
+TEST_F(AddOrderTest, NoTradeWhenPricesDoNotCross) {
     auto buy = createOrder(1, 150.00, 100, "BUY");
     auto sell = createOrder(2, 151.00, 100, "SELL");
 
@@ -82,7 +81,7 @@ TEST_F(OrderBookTest, NoTradeWhenPricesDoNotCross) {
               (std::vector<std::pair<price_t, quantity_t>>{{151.00, 100}}));
 }
 
-TEST_F(OrderBookTest, FullyMatchedOrdersAreRemoved) {
+TEST_F(AddOrderTest, FullyMatchedOrdersAreRemoved) {
     auto sell = createOrder(1, 150.00, 100, "SELL");
     auto buy = createOrder(2, 150.00, 100, "BUY");
 
@@ -93,7 +92,7 @@ TEST_F(OrderBookTest, FullyMatchedOrdersAreRemoved) {
     EXPECT_TRUE(orderBook.getPriceLevels("SELL").empty());
 }
 
-TEST_F(OrderBookTest, IncomingOrderCanBePartiallyFilled) {
+TEST_F(AddOrderTest, IncomingOrderCanBePartiallyFilled) {
     auto sell = createOrder(1, 150.00, 100, "SELL");
     auto buy = createOrder(2, 150.00, 200, "BUY");
 
