b2c8833b929e2d10e353085df19bf47c8efc43fd
Han <hanhhan2705@gmail.com>
2026-07-27 15:12:47 +1000

    test: setup a fixture to simplify createOrder

--- tests/orderbook/OrderBookTests.cpp
+++ tests/orderbook/OrderBookTests.cpp
@@ -1,17 +1,24 @@
 #include "OrderBook.hpp"
 #include <gtest/gtest.h>
 
-TEST(OrderBookTest, HighestBidIsReturned) {
+class OrderBookTest : public testing::Test {
+  protected:
+    OrderBookTest() = default;
+    Order createOrder(orderId_t id, price_t price, quantity_t quantity, side_t side) {
+        return Order(id, 1000000 + id, price, quantity, "AAPL", side);
+    }
+    OrderBook orderbook;
+};
+
+TEST_F(OrderBookTest, HighestBidIsReturned) {
     OrderBook book;
 
-    Order order1(1, 1000000, 150.25, 100, "AAPL", "BUY");
-
-    Order order2(2, 1000001, 150.30, 100, "AAPL", "BUY");
+    Order order1 = createOrder(1, 150.25, 100, "BUY");
+    Order order2 = createOrder(2, 150.30, 100, "BUY");
 
     book.addOrder(order1);
     book.addOrder(order2);
 
     std::vector<std::pair<price_t, quantity_t>> expected{{150.30, 100}, {150.25, 100}};
-
     EXPECT_EQ(book.getPriceLevels("BUY"), expected);
 }
