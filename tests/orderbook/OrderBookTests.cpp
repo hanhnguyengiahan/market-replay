ad0de12993cdff6969c256b2adde2f68205e7c39
Han <hanhhan2705@gmail.com>
2026-07-27 15:26:34 +1000

    test: add more addOrder test for orderBook

--- tests/orderbook/OrderBookTests.cpp
+++ tests/orderbook/OrderBookTests.cpp
@@ -3,22 +3,51 @@
 
 class OrderBookTest : public testing::Test {
   protected:
-    OrderBookTest() = default;
     Order createOrder(orderId_t id, price_t price, quantity_t quantity, side_t side) {
         return Order(id, 1000000 + id, price, quantity, "AAPL", side);
     }
-    OrderBook orderbook;
+    OrderBook orderBook;
 };
 
-TEST_F(OrderBookTest, HighestBidIsReturned) {
-    OrderBook book;
+TEST_F(OrderBookTest, EmptyBookHasNoPriceLevels) {
+    EXPECT_TRUE(orderBook.getPriceLevels("BUY").empty());
+    EXPECT_TRUE(orderBook.getPriceLevels("SELL").empty());
+}
 
+TEST_F(OrderBookTest, BuyAndSellOrdersStoredSeparately) {
+    auto buy = createOrder(1, 150.25, 100, "BUY");
+    auto sell = createOrder(2, 150.30, 100, "SELL");
+
+    orderBook.addOrder(buy);
+    orderBook.addOrder(sell);
+
+    EXPECT_EQ(orderBook.getPriceLevels("BUY"),
+              (std::vector<std::pair<price_t, quantity_t>>{{150.25, 100}}));
+
+    EXPECT_EQ(orderBook.getPriceLevels("SELL"),
+              (std::vector<std::pair<price_t, quantity_t>>{{150.30, 100}}));
+}
+
+TEST_F(OrderBookTest, QuantityAccumulatedIfSamePrice) {
+    auto order1 = createOrder(1, 150.25, 100, "BUY");
+    auto order2 = createOrder(2, 150.25, 100, "BUY");
+    auto order3 = createOrder(3, 150.30, 100, "BUY");
+
+    orderBook.addOrder(order1);
+    orderBook.addOrder(order2);
+    orderBook.addOrder(order3);
+
+    EXPECT_EQ(orderBook.getPriceLevels("BUY"),
+              (std::vector<std::pair<price_t, quantity_t>>{{150.30, 100}, {150.25, 200}}));
+}
+
+TEST_F(OrderBookTest, HighestBidsReturnFromPriceLevels) {
     Order order1 = createOrder(1, 150.25, 100, "BUY");
     Order order2 = createOrder(2, 150.30, 100, "BUY");
 
-    book.addOrder(order1);
-    book.addOrder(order2);
+    orderBook.addOrder(order1);
+    orderBook.addOrder(order2);
 
     std::vector<std::pair<price_t, quantity_t>> expected{{150.30, 100}, {150.25, 100}};
-    EXPECT_EQ(book.getPriceLevels("BUY"), expected);
+    EXPECT_EQ(orderBook.getPriceLevels("BUY"), expected);
 }
