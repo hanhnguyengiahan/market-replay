91cc67d02915037b36de7106b1e461b5bbc36eef
Han <hanhhan2705@gmail.com>
2026-07-29 15:39:46 +1000

    fix: change the sizes of each component

--- include/Renderer.hpp
+++ include/Renderer.hpp
@@ -2,12 +2,12 @@
 #include "Application.hpp"
 #include <ftxui/dom/elements.hpp>
 
-constexpr int APP_WIDTH = 130;
-constexpr int ORDERBOOK_TOTAL_WIDTH = 40;
-constexpr int REPLAY_INFO_TOTAL_WIDTH = 80;
+constexpr int APP_WIDTH = 100;
+constexpr int ORDERBOOK_TOTAL_WIDTH = 50;
+constexpr int REPLAY_INFO_TOTAL_WIDTH = 40;
 constexpr int MENU_TOTAL_WIDTH = 10;
 
-constexpr int APP_HEIGHT = 15;
+constexpr int APP_HEIGHT = 17;
 
 ftxui::Element renderReplayInfo(Application& app);
 ftxui::Element renderOrderBook(Application& app);
